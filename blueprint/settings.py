73c831d4db0bcfda504f4d79879b614c2917c233
builtbyjb <ajibolaawotide@gmail.com>
2024-12-03 15:30:35 -0500

    minor fix

--- blueprint/settings.py
+++ blueprint/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = debug
 
 ALLOWED_HOSTS = ['*']
 
